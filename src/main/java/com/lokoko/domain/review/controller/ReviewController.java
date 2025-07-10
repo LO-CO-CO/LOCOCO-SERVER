4f6fbbe9fafcf6323295e714db951e722bfcc814
rookie <piresj0318@gmail.com>
2025-07-10 22:44:03 +0900

    MOD #62 : 제품 상세 페이지의 사진 리뷰를 조회하는 컨트롤러 구현

--- src/main/java/com/lokoko/domain/review/controller/ReviewController.java
+++ src/main/java/com/lokoko/domain/review/controller/ReviewController.java
@@ -2,8 +2,8 @@ package com.lokoko.domain.review.controller;
 
 import com.lokoko.domain.review.controller.enums.ResponseMessage;
 import com.lokoko.domain.review.dto.request.ReviewReceiptRequest;
+import com.lokoko.domain.review.dto.response.ImageReviewsProductDetailResponse;
 import com.lokoko.domain.review.dto.response.ReviewReceiptResponse;
-import com.lokoko.domain.review.dto.response.TempResponse;
 import com.lokoko.domain.review.service.ReviewService;
 import com.lokoko.global.common.response.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -36,14 +36,15 @@ public class ReviewController {
     }
 
     @GetMapping("/details/image")
-    // 제품 상세 조회페이지에서, 유저의 사진 리뷰 조회해야함
-    public ApiResponse<TempResponse> getImageReviewsInProductDetail(@RequestParam Long productId) {
+    public ApiResponse<ImageReviewsProductDetailResponse> getImageReviewsInProductDetail(
+            @RequestParam Long productId,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "5") int size
+    ) {
+        ImageReviewsProductDetailResponse response = reviewService.getImageReviewsInProductDetail(productId, page,
+                size);
 
-        TempResponse response = reviewService.getImageReviewsInProductDetail(productId);
-
-        return ApiResponse.success(HttpStatus.OK, ResponseMessage.REVIEW_RECEIPT_PRESIGNED_URL_SUCCESS.getMessage(),
+        return ApiResponse.success(HttpStatus.OK, ResponseMessage.IMAGE_REVIEW_GET_SUCCESS.getMessage(),
                 response);
-
-
     }
 }
