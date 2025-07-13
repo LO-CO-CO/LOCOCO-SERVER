3e60534fce9675715f11c442a20a6abdc6683161
Hyoeun <joohyoeun@sookmyung.ac.kr>
2025-07-14 00:37:16 +0900

    mod #80 : 팩토리 메소드 제거

--- src/main/java/com/lokoko/domain/review/dto/response/VideoReviewProductDetail.java
+++ src/main/java/com/lokoko/domain/review/dto/response/VideoReviewProductDetail.java
@@ -1,8 +1,5 @@
 package com.lokoko.domain.review.dto.response;
 
-import com.lokoko.domain.review.entity.Review;
-import com.lokoko.domain.video.entity.ReviewVideo;
-
 public record VideoReviewProductDetail(
         Long reviewId,
         String brandName,
@@ -10,16 +7,5 @@ public record VideoReviewProductDetail(
         int likeCount,
         String videoUrl
 ) {
-
-    public static VideoReviewProductDetail from(ReviewVideo reviewVideo) {
-        Review review = reviewVideo.getReview();
-        return new VideoReviewProductDetail(
-                review.getId(),
-                review.getProduct().getBrandName(),
-                review.getProduct().getProductName(),
-                review.getLikeCount(),
-                reviewVideo.getMediaFile().getFileUrl()
-        );
-    }
 }
 
