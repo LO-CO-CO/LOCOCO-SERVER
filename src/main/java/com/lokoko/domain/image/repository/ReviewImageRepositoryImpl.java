9f38c0a0a2cda5166c156b5ac2b907681df5458d
Hyoeun <joohyoeun@sookmyung.ac.kr>
2025-07-14 03:21:46 +0900

    mod #80 : Image/Video 정렬 로직 수정

--- src/main/java/com/lokoko/domain/image/repository/ReviewImageRepositoryImpl.java
+++ src/main/java/com/lokoko/domain/image/repository/ReviewImageRepositoryImpl.java
@@ -1,10 +1,12 @@
 package com.lokoko.domain.image.repository;
 
 import com.lokoko.domain.image.entity.QReviewImage;
-import com.lokoko.domain.image.entity.ReviewImage;
 import com.lokoko.domain.like.entity.QReviewLike;
 import com.lokoko.domain.product.entity.QProduct;
+import com.lokoko.domain.review.dto.response.MainImageReview;
 import com.lokoko.domain.review.entity.QReview;
+import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
@@ -22,21 +24,26 @@ public class ReviewImageRepositoryImpl implements ReviewImageRepositoryCustom {
     private static final QReviewLike reviewLike = QReviewLike.reviewLike;
 
     @Override
-    public List<ReviewImage> findMainImageReviewSorted() {
+    public List<MainImageReview> findMainImageReviewSorted() {
         return queryFactory
-                .selectFrom(reviewImage)
-                .join(reviewImage.review, review).fetchJoin()
-                .join(review.product, product).fetchJoin()
+                .select(Projections.constructor(MainImageReview.class,
+                        review.id,
+                        product.brandName,
+                        product.productName,
+                        reviewLike.count().intValue(),
+                        // 일단 여기서 rank 0, service에서 추가
+                        Expressions.constant(0),
+                        reviewImage.mediaFile.fileUrl
+                ))
+                .from(reviewImage)
+                .join(reviewImage.review, review)
+                .join(review.product, product)
                 .leftJoin(reviewLike).on(reviewLike.review.eq(review))
-                // 대표 이미지 조건: displayOrder == 0
                 .where(reviewImage.displayOrder.eq(0))
-                .groupBy(reviewImage.id, review.id, product.id, review.rating)
-                // 정렬 조건: 좋아요 내림차순 → 평점 내림차순
-                .orderBy(
-                        reviewLike.count().desc(),
-                        review.rating.desc()
-                )
+                .groupBy(review.id, product.brandName, product.productName, reviewImage.mediaFile.fileUrl)
+                .orderBy(reviewLike.count().desc(), review.rating.desc())
                 .limit(4)
                 .fetch();
     }
+
 }
