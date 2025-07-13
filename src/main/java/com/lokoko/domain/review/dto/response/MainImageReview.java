0faadfe2d762b8e6afe83e25e3d42777d3341af1
Hyoeun <joohyoeun@sookmyung.ac.kr>
2025-07-14 03:21:07 +0900

    mod #80 : DTO에서 팩토리 메소드 제거

--- src/main/java/com/lokoko/domain/review/dto/response/MainImageReview.java
+++ src/main/java/com/lokoko/domain/review/dto/response/MainImageReview.java
@@ -1,8 +1,5 @@
 package com.lokoko.domain.review.dto.response;
 
-import com.lokoko.domain.image.entity.ReviewImage;
-import com.lokoko.domain.review.entity.Review;
-
 public record MainImageReview(
         Long reviewId,
         String brandName,
@@ -11,19 +8,6 @@ public record MainImageReview(
         int rank,
         String reviewImage
 ) {
-
-    public static MainImageReview from(ReviewImage reviewImage, int rank) {
-        Review review = reviewImage.getReview();
-
-        return new MainImageReview(
-                review.getId(),
-                review.getProduct().getBrandName(),
-                review.getProduct().getProductName(),
-                review.getLikeCount(),
-                rank,
-                reviewImage.getMediaFile().getFileUrl()
-        );
-    }
 }
 
 
