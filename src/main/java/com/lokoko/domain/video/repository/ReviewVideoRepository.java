51a034bda7d94e56cff2e8ac6cc5334e42f7bf64
huncozyboy <leejh5838@naver.com>
2025-07-14 04:17:06 +0900

    FEAT #86 : 리뷰 비디오 레포에서 findAllByReviewId 메서드 추가

--- src/main/java/com/lokoko/domain/video/repository/ReviewVideoRepository.java
+++ src/main/java/com/lokoko/domain/video/repository/ReviewVideoRepository.java
@@ -2,10 +2,13 @@ package com.lokoko.domain.video.repository;
 
 import com.lokoko.domain.review.entity.Review;
 import com.lokoko.domain.video.entity.ReviewVideo;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ReviewVideoRepository extends JpaRepository<ReviewVideo, Long>, ReviewVideoRepositoryCustom {
     void deleteAllByReview(Review review);
+
+    List<ReviewVideo> findAllByReviewId(Long reviewId);
 }
