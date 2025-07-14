514d4aa9c57f9e58889639085816d95102e5077e
huncozyboy <leejh5838@naver.com>
2025-07-15 08:48:52 +0900

    MOD #105 : 쿠키 config 설정 분리

--- src/main/java/com/lokoko/global/auth/jwt/utils/CookieUtil.java
+++ src/main/java/com/lokoko/global/auth/jwt/utils/CookieUtil.java
@@ -1,30 +1,23 @@
 package com.lokoko.global.auth.jwt.utils;
 
+import com.lokoko.global.config.CookieConfig;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.beans.factory.annotation.Value;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseCookie;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class CookieUtil {
-    @Value("${lokoko.jwt.cookieMaxAge}")
-    private Long cookieMaxAge;
 
-    @Value("${lokoko.jwt.secureOption}")
-    private boolean secureOption;
-
-    @Value("${lokoko.jwt.cookiePathOption}")
-    private String cookiePathOption;
-
-    @Value("${lokoko.jwt.cookieDomain}")
-    private String cookieDomain;
+    private final CookieConfig cookieConfig;
 
     public void setCookie(String name, String value, HttpServletResponse response) {
         ResponseCookie cookie = ResponseCookie.from(name, value)
-                .domain(cookieDomain)
-                .maxAge(cookieMaxAge)
-                .path(cookiePathOption)
-                .secure(secureOption)
+                .domain(cookieConfig.getCookieDomain())
+                .maxAge(cookieConfig.getCookieMaxAge())
+                .path(cookieConfig.getCookiePathOption())
+                .secure(cookieConfig.isSecureOption())
                 .httpOnly(true)
                 .sameSite("none")
                 .build();
@@ -34,10 +27,10 @@ public class CookieUtil {
 
     public void deleteCookie(HttpServletResponse response, String name) {
         ResponseCookie cookie = ResponseCookie.from(name, "value")
-                .domain(cookieDomain)
+                .domain(cookieConfig.getCookieDomain())
                 .maxAge(0)
-                .path(cookiePathOption)
-                .secure(secureOption)
+                .path(cookieConfig.getCookiePathOption())
+                .secure(cookieConfig.isSecureOption())
                 .httpOnly(true)
                 .sameSite("none")
                 .build();
