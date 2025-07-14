3699d2a33eabaa3ba156457b941d8568539231e1
huncozyboy <leejh5838@naver.com>
2025-07-15 05:26:22 +0900

    MOD #101 : getOptionalUrl 분기처리 URL 추가

--- src/main/java/com/lokoko/global/config/PermitUrlConfig.java
+++ src/main/java/com/lokoko/global/config/PermitUrlConfig.java
@@ -5,28 +5,65 @@ import org.springframework.stereotype.Component;
 @Component
 public class PermitUrlConfig {
 
+    /**
+     * 1) 전체 공개 (Public) - 토큰, @CurrentUser 필요 없음
+     */
     public String[] getPublicUrl() {
         return new String[]{
                 "/swagger-ui/**",
                 "/v3/api-docs/**",
                 "/api/auth/**",
-                "/api/reviews/{productId}/{userId}"
+                "/api/youtube/trends",
+                "/api/reviews/image",
+                "/api/reviews/video",
+                "/api/reviews/details/image",
+                "/api/reviews/details/video",
+                "/api/reviews/{productId}/{userId}",
+                "/api/products/details/{productId}/youtube"
         };
     }
 
+    /**
+     * 2) 선택 인증 (Optional Auth) - 토큰 있으면 읽어서 @CurrentUser 주입, 없어도 접근은 가능
+     */
+    public String[] getOptionalUrl() {
+        return new String[]{
+                "/api/reviews/{reviewId}/image",
+                "/api/reviews/{reviewId}/video",
+                "/api/reviews/details/{reviewId}/image",
+                "/api/reviews/details/{reviewId}/video",
+                "/api/products/categories/search",
+                "/api/products/search",
+                "/api/products/categories/new",
+                "/api/products/categories/popular",
+                "/api/products/details/{productId}",
+        };
+    }
+
+    /**
+     * 3) 인증 필요 (User) - 회원가입한 유저만 접근 가능 (둘다 필요)
+     */
     public String[] getUserUrl() {
         return new String[]{
+                "/api/likes/**",
+                "/api/reviews/{productId}",
+                "/api/reviews/media",
+                "/api/reviews/receipt"
         };
     }
 
+    /**
+     * 4) 관리자 전용 (Admin) - ROLE_ADMIN
+     */
     public String[] getAdminUrl() {
         return new String[]{
                 "/api/admin/**",
                 "/api/products/crawl",
-                "/api/youtube/{productId}/crawl",
-                "/api/youtube/trends/crawl",
                 "/api/products/crawl/new",
-                "/api/products/crawl/options"
+                "/api/products/crawl/options",
+                "/api/products/search-fields/migrate",
+                "/api/youtube/{productId}/crawl",
+                "/api/youtube/trends/crawl"
         };
     }
 
