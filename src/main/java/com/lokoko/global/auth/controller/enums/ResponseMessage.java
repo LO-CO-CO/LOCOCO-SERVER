0cfc5632dac5add36a9d726418b5aa564851c834
huncozyboy <leejh5838@naver.com>
2025-07-15 08:49:41 +0900

    MOD #105 : 로그아웃 컨트롤러 로직 구현

--- src/main/java/com/lokoko/global/auth/controller/enums/ResponseMessage.java
+++ src/main/java/com/lokoko/global/auth/controller/enums/ResponseMessage.java
@@ -7,6 +7,7 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ResponseMessage {
     LOGIN_SUCCESS("회원가입/로그인 성공에 성공했습니다."),
+    LOGOUT_SUCCESS("로그아웃에 성공했습니다."),
     URL_GET_SUCCESS("리다이렉트 URL 조회에 성공했습니다."),
     REFRESH_TOKEN_REISSUE("리프레시 토큰 재발급에 성공했습니다.");
 
