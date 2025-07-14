5644df6f65708fa3b6d25370cd9e38ed52c0c8c8
rookie <piresj0318@gmail.com>
2025-07-14 19:13:21 +0900

    MOD : 메인 이미지 리뷰 레코드에 스키마 어노테이션 추가

--- src/main/java/com/lokoko/domain/review/dto/response/MainImageReview.java
+++ src/main/java/com/lokoko/domain/review/dto/response/MainImageReview.java
@@ -1,11 +1,25 @@
 package com.lokoko.domain.review.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public record MainImageReview(
+
+        @Schema(description = "리뷰 아이디")
         Long reviewId,
+
+        @Schema(description = "브랜드 이름")
         String brandName,
+
+        @Schema(description = "상품 이름")
         String productName,
+
+        @Schema(description = "리뷰 좋아요 개수")
         int likeCount,
+
+        @Schema(description = "리뷰 순위")
         int rank,
+
+        @Schema(description = "리뷰 이미지")
         String reviewImage
 ) {
 }
