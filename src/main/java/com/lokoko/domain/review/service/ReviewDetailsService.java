7be304ac04830fbd73690f42277256c06fdce7ac
rookie <piresj0318@gmail.com>
2025-07-14 15:18:37 +0900

    FEAT #87 : 사진 리뷰 상세 조회 기능 구현

--- src/main/java/com/lokoko/domain/review/service/ReviewDetailsService.java
+++ src/main/java/com/lokoko/domain/review/service/ReviewDetailsService.java
@@ -1,14 +1,20 @@
 package com.lokoko.domain.review.service;
 
+import com.lokoko.domain.image.entity.ReviewImage;
+import com.lokoko.domain.image.repository.ReviewImageRepository;
 import com.lokoko.domain.like.repository.ReviewLikeRepository;
+import com.lokoko.domain.review.dto.response.ImageReviewDetailResponse;
 import com.lokoko.domain.review.dto.response.VideoReviewDetailResponse;
+import com.lokoko.domain.review.entity.Review;
 import com.lokoko.domain.review.exception.ReviewNotFoundException;
 import com.lokoko.domain.review.exception.ReviewVideoNotFoundException;
 import com.lokoko.domain.review.repository.ReviewRepository;
+import com.lokoko.domain.user.entity.User;
 import com.lokoko.domain.user.exception.UserNotFoundException;
 import com.lokoko.domain.user.repository.UserRepository;
 import com.lokoko.domain.video.entity.ReviewVideo;
 import com.lokoko.domain.video.repository.ReviewVideoRepository;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -21,6 +27,8 @@ public class ReviewDetailsService {
     private final ReviewRepository reviewRepository;
     private final ReviewVideoRepository reviewVideoRepository;
     private final ReviewLikeRepository reviewLikeRepository;
+    private final ReviewImageRepository reviewImageRepository;
+
 
     public VideoReviewDetailResponse getVideoReviewDetails(Long reviewId,
                                                            Long userId) {
@@ -36,5 +44,23 @@ public class ReviewDetailsService {
         long totalLikes = reviewLikeRepository.countByReviewId(reviewId);
 
         return VideoReviewDetailResponse.from(video, totalLikes);
+    }
+
+
+    public ImageReviewDetailResponse getImageReviewDetails(Long reviewId, Long userId) {
+
+        User author = userRepository.findById(userId)
+                .orElseThrow(UserNotFoundException::new);
+
+        Review review = reviewRepository.findById(reviewId)
+                .orElseThrow(ReviewNotFoundException::new);
+
+        List<ReviewImage> reviewImages = reviewImageRepository.findByReviewId(reviewId);
+
+        long totalLikes = reviewLikeRepository.countByReviewId(reviewId);
+
+        return ImageReviewDetailResponse.from(author, review, reviewImages, totalLikes);
+
+
     }
 }
