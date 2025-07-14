d47ae930dd8b248dffae695be10e5bfa01e8c086
Hyoeun <joohyoeun@sookmyung.ac.kr>
2025-07-15 00:39:28 +0900

    mod #99 : Primitive 타입으로 변경

--- src/main/java/com/lokoko/domain/like/dto/response/ToggleLikeResponse.java
+++ src/main/java/com/lokoko/domain/like/dto/response/ToggleLikeResponse.java
@@ -6,6 +6,6 @@ import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.REQUIRED;
 
 public record ToggleLikeResponse(
         @Schema(requiredMode = REQUIRED)
-        boolean isLiked
+        Boolean isLiked
 ) {
 }
