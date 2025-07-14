d698e95ac9f669c90852151e2493062512bebb31
Hyoeun <joohyoeun@sookmyung.ac.kr>
2025-07-15 02:50:18 +0900

    mod #99 : line dto에 requiredMode 추가

--- src/main/java/com/lokoko/global/auth/line/dto/LineLoginResponse.java
+++ src/main/java/com/lokoko/global/auth/line/dto/LineLoginResponse.java
@@ -2,8 +2,12 @@ package com.lokoko.global.auth.line.dto;
 
 import com.lokoko.global.auth.entity.enums.OauthLoginStatus;
 import com.lokoko.global.auth.jwt.dto.LoginResponse;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.REQUIRED;
 
 public record LineLoginResponse(
+        @Schema(requiredMode = REQUIRED)
         OauthLoginStatus loginStatus
 ) {
     public static LineLoginResponse of(
