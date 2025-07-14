2ae6015c59d95aac56a526d2ce2b5d364f799c28
rookie <piresj0318@gmail.com>
2025-07-14 22:37:31 +0900

    FEAT #97 : 영수증 이미지가 존재하지 않는 경우에 대한 예외 추가

--- src/main/java/com/lokoko/domain/review/exception/ErrorMessage.java
+++ src/main/java/com/lokoko/domain/review/exception/ErrorMessage.java
@@ -16,7 +16,8 @@ public enum ErrorMessage {
     TOO_MANY_IMAGE_FILES("image는 최대 5개까지만 업로드할 수 있습니다."),
     INVALID_PRESIGNED_URL("Presigned URL 파싱에 실패했습니다."),
     REVIEW_NOT_FOUND("존재하지 않는 리뷰입니다."),
-    REVIEW_VIDEO_NOT_FOUND("존재하지 않는 리뷰 영상입니다.");
+    REVIEW_VIDEO_NOT_FOUND("존재하지 않는 리뷰 영상입니다."),
+    RECEIPT_IMAGE_NOT_FOUND("존재하지 않는 영수증 입니다");
 
     private final String message;
 }
