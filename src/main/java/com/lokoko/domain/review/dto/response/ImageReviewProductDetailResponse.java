5703fb4a78eddf52da32872fc8cb18cd8b31da41
rookie <piresj0318@gmail.com>
2025-07-15 17:07:23 +0900

    MOD #109 : 제품 상세페이지에서 사진 리뷰 조회 응답 DTO 의 필드 수정
    1. 추후 리뷰 작성자 상세조회를 위한 authorId 필드 추가
    
    2. 현재 서비스 사용자가 좋아요 눌렀는지 여부를 확인하기 위한 isLiked 필드 추가
    
    3. 현재 서비스 사용자가 자신이 작성한 리뷰인지 여부를 확인하기 위한 isMine 필드 추가

--- src/main/java/com/lokoko/domain/review/dto/response/ImageReviewProductDetailResponse.java
+++ src/main/java/com/lokoko/domain/review/dto/response/ImageReviewProductDetailResponse.java
@@ -1,12 +1,11 @@
 package com.lokoko.domain.review.dto.response;
 
-import io.swagger.v3.oas.annotations.media.Schema;
+import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.REQUIRED;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-
-import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.REQUIRED;
 
 public record ImageReviewProductDetailResponse(
         @Schema(requiredMode = REQUIRED)
@@ -22,31 +21,40 @@ public record ImageReviewProductDetailResponse(
         @Schema(requiredMode = REQUIRED)
         String authorName,
         @Schema(requiredMode = REQUIRED)
+        Long authorId,
+        @Schema(requiredMode = REQUIRED)
         Double rating,
         @Schema(requiredMode = REQUIRED)
         String option,
         @Schema(requiredMode = REQUIRED)
         Integer likeCount,
         @Schema(requiredMode = REQUIRED)
-        List<String> images
+        List<String> images,
+        @Schema(requiredMode = REQUIRED)
+        Boolean isLiked,
+        @Schema(requiredMode = REQUIRED)
+        Boolean isMine
 ) {
 
     public ImageReviewProductDetailResponse(Long reviewId, LocalDateTime writtenTime,
                                             Boolean receiptUploaded, String positiveComment,
                                             String negativeComment, String authorName,
-                                            Double rating, String option,
-                                            Integer likeCount, List<String> images) {
+                                            Long authorId, Double rating, String option,
+                                            Integer likeCount, List<String> images,
+                                            Boolean isLiked, Boolean isMine
+    ) {
         this.reviewId = reviewId;
         this.writtenTime = writtenTime;
         this.receiptUploaded = receiptUploaded;
         this.positiveComment = positiveComment;
         this.negativeComment = negativeComment;
         this.authorName = authorName;
+        this.authorId = authorId;
         this.rating = rating;
         this.option = option;
         this.likeCount = likeCount;
-        this.images = new ArrayList<>(images);
+        this.images = images != null ? new ArrayList<>(images) : new ArrayList<>();
+        this.isLiked = isLiked;
+        this.isMine = isMine;
     }
-
-
 }
