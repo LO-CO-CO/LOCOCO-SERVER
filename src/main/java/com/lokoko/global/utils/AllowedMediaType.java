845719420fb62f102f86896e79689a8e71c5861a
Hyoeun <joohyoeun@sookmyung.ac.kr>
2025-07-15 15:03:50 +0900

    mod #107 : video/mov 타입 추가했습니다

--- src/main/java/com/lokoko/global/utils/AllowedMediaType.java
+++ src/main/java/com/lokoko/global/utils/AllowedMediaType.java
@@ -14,6 +14,7 @@ public final class AllowedMediaType {
             "video/mp4",
             "video/avi",
             "video/mkv",
+            "video/mov",
             "video.quicktime"
     );
 }
