ea2417e15e8cb8d47fb3a372d810e6e728297523
rookie <piresj0318@gmail.com>
2025-07-15 16:00:07 +0900

    FEAT #109 : 사용자가 좋아요 눌렀는지 확인 할 수 있는 메소드 구현

--- src/main/java/com/lokoko/domain/like/repository/ReviewLikeRepository.java
+++ src/main/java/com/lokoko/domain/like/repository/ReviewLikeRepository.java
@@ -1,6 +1,8 @@
 package com.lokoko.domain.like.repository;
 
 import com.lokoko.domain.like.entity.ReviewLike;
+import com.lokoko.domain.review.entity.Review;
+import com.lokoko.domain.user.entity.User;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -10,4 +12,6 @@ public interface ReviewLikeRepository extends JpaRepository<ReviewLike, Long> {
     Optional<ReviewLike> findByReviewIdAndUserId(Long reviewId, Long userId);
 
     long countByReviewId(Long reviewId);
+
+    boolean existsByUserAndReview(User user, Review review);
 }
