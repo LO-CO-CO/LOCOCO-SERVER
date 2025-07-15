74ca8da3daa765e55c9d131b11c6975a84ef6f1a
rookie <piresj0318@gmail.com>
2025-07-15 15:59:41 +0900

    FEAT #109 : ProductImage 조회 메소드 추가

--- src/main/java/com/lokoko/domain/image/repository/ProductImageRepository.java
+++ src/main/java/com/lokoko/domain/image/repository/ProductImageRepository.java
@@ -1,7 +1,9 @@
 package com.lokoko.domain.image.repository;
 
 import com.lokoko.domain.image.entity.ProductImage;
+import com.lokoko.domain.product.entity.Product;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +11,6 @@ import org.springframework.stereotype.Repository;
 public interface ProductImageRepository extends JpaRepository<ProductImage, Long> {
 
     List<ProductImage> findByProductIdIn(List<Long> productIds);
+
+    Optional<ProductImage> findByProduct(Product product);
 }
