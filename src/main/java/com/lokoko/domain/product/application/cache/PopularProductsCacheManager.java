7a96a353faaba5dda7847b52aea2b897b4d3685c
rookie <piresj0318@gmail.com>
2025-08-06 20:35:09 +0900

    FEAT #172 : 캐시 무효화를 위한 별도의 클래스 도입
    메인 페이지에서 인기 상품 조회에 대한 캐싱의 무효화 시점은, "리뷰가 작성되거나 삭제되는 시점" 입니다.
    
    왜냐하면 인기 상품 조회에는 상품에 대한 리뷰 수 , 별점이 표시 되기 때문에 ,
    상품에 대해 새로운 리뷰가 작성되거나 삭제되면
    데이터가 업데이트 되어야합니다.
    
    즉, 기존에 캐싱된 값을 무효화해야하는 것입니다.

---
+++ src/main/java/com/lokoko/domain/product/application/cache/PopularProductsCacheManager.java
@@ -0,0 +1,23 @@
+package com.lokoko.domain.product.application.cache;
+
+import com.lokoko.domain.product.domain.entity.enums.MiddleCategory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class PopularProductsCacheManager {
+
+    @CacheEvict(value = "popularProducts", key = "#category.name() + ':top4'")
+    public void evictCategoryCache(MiddleCategory category) {
+
+    }
+
+    @CacheEvict(value = "popularProducts", allEntries = true)
+    public void evictAllPopularProductsCache() {
+
+    }
+
+
+}
