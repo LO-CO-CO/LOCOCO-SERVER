961d37e391e502e415826b5e4d978a3bb902f707
rookie <piresj0318@gmail.com>
2025-08-06 20:40:34 +0900

    FEAT #172 : 메인페이지 인기상품 조회 캐싱을 담당하는 서비스 추가

---
+++ src/main/java/com/lokoko/domain/product/application/service/ProductCacheService.java
@@ -0,0 +1,39 @@
+package com.lokoko.domain.product.application.service;
+
+import com.lokoko.domain.product.api.dto.PopularProductProjection;
+import com.lokoko.domain.product.api.dto.response.CachedPopularProductsResponse;
+import com.lokoko.domain.product.domain.entity.enums.MiddleCategory;
+import com.lokoko.domain.product.domain.repository.ProductRepository;
+import com.lokoko.domain.product.mapper.ProductMapper;
+import com.lokoko.global.common.response.PageableResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ProductCacheService {
+
+    private final ProductRepository productRepository;
+    private final ProductMapper productMapper;
+
+
+    @Cacheable(value = "popularProducts", key = "#middleCategory.name() + ':top4'")
+    public CachedPopularProductsResponse getPopularProductsFromCache(MiddleCategory middleCategory) {
+
+        Pageable pageable = PageRequest.of(0, 4);
+
+        Slice<PopularProductProjection> projectionSlice =
+                productRepository.findPopularProductsWithDetails(middleCategory, null, pageable);
+
+        return productMapper.toCachedPopularProductResponse(
+                projectionSlice.getContent(),
+                middleCategory,
+                PageableResponse.of(projectionSlice)
+        );
+    }
+
+}
