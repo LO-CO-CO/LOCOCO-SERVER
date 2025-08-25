c36105086f69cc0c693d5e64ef82e9063e695f18
Hyoeun <joohyoeun@sookmyung.ac.kr>
2025-08-26 01:09:15 +0900

    MOD #196 : 주소 필드, @NotBlank 추가

--- src/main/java/com/lokoko/domain/creator/domain/entity/Creator.java
+++ src/main/java/com/lokoko/domain/creator/domain/entity/Creator.java
@@ -1,14 +1,18 @@
 package com.lokoko.domain.creator.domain.entity;
 
+import com.lokoko.domain.creator.domain.entity.enums.CreatorLevel;
 import com.lokoko.domain.user.domain.entity.User;
-import com.lokoko.domain.user.domain.entity.enums.Address;
-import com.lokoko.domain.user.domain.entity.enums.Role;
+import com.lokoko.domain.user.domain.entity.enums.PersonalColor;
+import com.lokoko.domain.user.domain.entity.enums.SkinTone;
+import com.lokoko.domain.user.domain.entity.enums.SkinType;
 import jakarta.persistence.Column;
 import jakarta.persistence.DiscriminatorValue;
-import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.PrimaryKeyJoinColumn;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -23,27 +27,50 @@ import lombok.experimental.SuperBuilder;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Creator extends User {
 
+    @NotBlank
     @Column(nullable = false)
-    private String nickName;
+    private String creatorName;
 
-    @Embedded
-    private Address address;
+    @Enumerated(EnumType.STRING)
+    private PersonalColor personalColor;
+
+    @Enumerated(EnumType.STRING)
+    private SkinTone skinTone;
+
+    @Enumerated(EnumType.STRING)
+    private SkinType skinType;
+
+    @Enumerated(EnumType.STRING)
+    @Column
+    private CreatorLevel creatorLevel;
+
+    @NotBlank
+    @Column(nullable = false)
+    private String country;
+
+    @NotBlank
+    @Column(nullable = false)
+    private String postalCode;
+
+    // 주/도/광역시 등
+    @NotBlank
+    @Column(nullable = false)
+    private String stateOrProvince;
+
+    @NotBlank
+    @Column(nullable = false)
+    private String city;
+
+    @NotBlank
+    @Column(nullable = false)
+    private String addressLine1;
+
+    private String addressLine2;
 
     @Column
     private String instaLink;
 
     @Column
     private String tiktokLink;
-
-
-    public static Creator of(String email, String name, String nickName, Address address) {
-        return Creator.builder()
-                .email(email)
-                .name(name)
-                .nickName(nickName)
-                .address(address)
-                .role(Role.CREATOR)
-                .build();
-    }
 
 }
