1c7672351026a3b6a0ef224689137b27625322e4
Hyoeun <joohyoeun@sookmyung.ac.kr>
2025-08-30 12:27:50 +0900

    MOD #200 : Admin Entity 수정

--- src/main/java/com/lokoko/global/auth/entity/Admin.java
+++ src/main/java/com/lokoko/global/auth/entity/Admin.java
@@ -1,10 +1,11 @@
 package com.lokoko.global.auth.entity;
 
 import com.lokoko.domain.user.domain.entity.User;
-import com.lokoko.domain.user.domain.entity.enums.Role;
-import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
-import jakarta.persistence.PrimaryKeyJoinColumn;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.MapsId;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -14,17 +15,16 @@ import lombok.experimental.SuperBuilder;
 @Getter
 @Entity
 @Table(name = "admins")
-@DiscriminatorValue("ADMIN")
-@PrimaryKeyJoinColumn(name = "user_id")
 @SuperBuilder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Admin extends User {
+public class Admin {
 
-    public static Admin createAdmin(String email, String name) {
-        return Admin.builder()
-                .email(email)
-                .name(name)
-                .role(Role.ADMIN)
-                .build();
-    }
+    @Id
+    private Long id;
+
+    @OneToOne
+    @MapsId
+    @JoinColumn(name = "user_id")
+    private User user;
+
 }
