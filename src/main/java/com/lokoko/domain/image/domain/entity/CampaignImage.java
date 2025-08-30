5d36c7d926b5eeecfdded904a3fe72fa668bef5f
unrookie21 <piresj0318@gmail.com>
2025-08-30 18:32:43 +0900

    FEAT #203 : CampaignImage 엔티티 생성
    캠페인에 이미지가 여러개 포함될 수 있기 때문에,
    CampaignImage 엔티티를 생성해주었습니다.

---
+++ src/main/java/com/lokoko/domain/image/domain/entity/CampaignImage.java
@@ -0,0 +1,34 @@
+package com.lokoko.domain.image.domain.entity;
+
+import com.lokoko.domain.campagin.domain.entity.Campaign;
+import com.lokoko.global.common.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import static jakarta.persistence.FetchType.*;
+
+@Getter
+@Entity
+@SuperBuilder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class CampaignImage extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "campaign_image_id")
+    private Long id;
+
+    private String url;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "campaign_id")
+    private Campaign campaign;
+
+    @Column(name = "is_main")
+    private boolean isMain;
+
+
+}
