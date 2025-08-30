edfc7ec9c47910f2c7334c1e29bf86d57beda153
unrookie21 <piresj0318@gmail.com>
2025-08-30 18:38:03 +0900

    FEAT #203 : Campaign 엔티티 생성
    크리에이터 참여 보상 목록
    크리에이터 제출 콘텐츠 목록
    크리에이터 참여 조건 목록
    
    위 세가지는, 값 타입 컬렉션으로 구성하였습니다.
    
    위 세가지 필드의 특징을 살펴봅시다.
    하나의 필드에 여러가지 데이터가 들어갈 수 있습니다.
    이런 값을 String 에 한번에 저장하는 것은 무리가 있다고 판단했습니다. 또한, String 으로 한번에 저장했던 "콘텐츠 목록" 같은 데이터를 파싱해서 클라이언트에 전달하는 것에는 어려움이 있다고 생각했습니다.
    
    브랜드마다 해당 항목에 대한 입력 방식이 다를테니, 모든 케이스를 고려해서 파싱을 진행하기도 어려운 상황입니다.
    
    그래서, 값 타입 컬렉션으로 필드를 구성하였습니다.
    이 값 타입 컬렉션을, 엔티티로 승격할지는 함께 논의해보았으면 좋겠습니다.

---
+++ src/main/java/com/lokoko/domain/campagin/domain/entity/Campaign.java
@@ -0,0 +1,152 @@
+package com.lokoko.domain.campagin.domain.entity;
+
+
+import com.lokoko.domain.brand.domain.entity.Brand;
+import com.lokoko.domain.campagin.domain.entity.enums.CampaignStatus;
+import com.lokoko.domain.campagin.domain.entity.enums.CampaignType;
+import com.lokoko.global.common.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import java.time.Instant;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static jakarta.persistence.FetchType.*;
+
+@Getter
+@Entity
+@SuperBuilder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Campaign extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "campaign_id")
+    private Long id;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "brand_id")
+    private Brand brand;
+
+    @Column(nullable = false)
+    private String title;
+
+    /**
+     * 캠페인 종류
+     * ex. GIVEAWAY / CONTENTS / EXCLUSIVE
+     */
+    @Enumerated(value = EnumType.STRING)
+    @Column(nullable = false)
+    private CampaignType campaignType;
+
+    /**
+     * 캠페인 상태
+     * ex. 임시 저장 / 대기 중 / 오픈 예정 / 진행 중 / 종료
+     */
+    @Enumerated(value = EnumType.STRING)
+    @Column(nullable = false)
+    private CampaignStatus campaignStatus;
+
+    @Column(nullable = false)
+    private Instant applyStartDate;
+
+    @Column(nullable = false)
+    private Instant applyDeadline;
+
+    @Column(nullable = false)
+    private Instant creatorAnnouncementDate;
+
+    @Column(nullable = false)
+    private Instant reviewSubmissionDeadline;
+
+    @Column(nullable = false)
+    private int recruitmentNumber;
+
+
+    /**
+     * 크리에이터 참여 보상 목록
+     */
+    @ElementCollection
+    @CollectionTable(
+            name = "campaign_participation_rewards",
+            joinColumns = @JoinColumn(name = "campaign_id")
+    )
+    private List<String> participationRewards;
+
+    /**
+     * 크리에이터 제출 콘텐츠 목록
+     */
+    @ElementCollection
+    @CollectionTable(
+            name = "campaign_deliverable_requirements",
+            joinColumns = @JoinColumn(name = "campaign_id")
+    )
+    private List<String> deliverableRequirements;
+
+    /**
+     * 크리에이터 참여 조건 목록
+     */
+    @ElementCollection
+    @CollectionTable(
+            name = "campaign_eligibility_requirements",
+            joinColumns = @JoinColumn(name = "campaign_id")
+    )
+    private List<String> eligibilityRequirements;
+
+
+    /**
+     * 캠페인 상태 변경
+     * @param newStatus
+     */
+    public void changeStatus(CampaignStatus newStatus) {
+        this.campaignStatus = newStatus;
+    }
+
+    /**
+     * 임시 저장 여부 반환
+     * 필수 필드 중 하나라도 비어있으면 임시저장으로 간주.
+     * @return 임시저장 여부
+     */
+    public boolean isDraft() {
+        return Stream.of(
+                this.brand == null,
+                this.title == null || this.title.isBlank(),
+                this.campaignType == null,
+                this.applyStartDate == null,
+                this.applyDeadline == null,
+                this.creatorAnnouncementDate == null,
+                this.reviewSubmissionDeadline == null,
+                this.recruitmentNumber <= 0,
+                this.participationRewards == null || this.participationRewards.isEmpty(),
+                this.deliverableRequirements == null || this.deliverableRequirements.isEmpty(),
+                this.eligibilityRequirements == null || this.eligibilityRequirements.isEmpty()
+        ).anyMatch(condition -> condition);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
