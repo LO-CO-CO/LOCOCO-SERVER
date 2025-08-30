ac7f43a1865fcfb6d70694aeb3c592842c16965e
Hyoeun <joohyoeun@sookmyung.ac.kr>
2025-08-30 12:24:38 +0900

    MOD #200 : Customer 엔티티 수정

--- src/main/java/com/lokoko/domain/customer/domain/entity/Customer.java
+++ src/main/java/com/lokoko/domain/customer/domain/entity/Customer.java
@@ -1,51 +1,32 @@
 package com.lokoko.domain.customer.domain.entity;
 
 import com.lokoko.domain.user.domain.entity.User;
-import com.lokoko.domain.user.domain.entity.enums.Role;
-import jakarta.persistence.Column;
-import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
-import jakarta.persistence.PrimaryKeyJoinColumn;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.MapsId;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.experimental.SuperBuilder;
-
-import java.time.Instant;
 
 @Getter
 @Entity
 @Table(name = "customers")
-@DiscriminatorValue("CUSTOMER")
-@PrimaryKeyJoinColumn(name = "user_id")
-@SuperBuilder
+@Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Customer extends User {
+@AllArgsConstructor
+public class Customer {
 
-    @Column(name = "line_id", unique = true)
-    private String lineId;
+    @Id
+    private Long id;
 
-    @Column(name = "google_id", unique = true)
-    private String googleId;
+    @OneToOne
+    @MapsId
+    @JoinColumn(name = "user_id")
+    private User user;
 
-    public static Customer createLineUser(String lineUserId, String email, String displayName) {
-        return Customer.builder()
-                .lineId(lineUserId)
-                .email(email)
-                .name(displayName)
-                .role(Role.CUSTOMER)
-                .lastLoginAt(Instant.now())
-                .build();
-    }
-
-    public static Customer createGoogleUser(String googleUserId, String email, String displayName) {
-        return Customer.builder()
-                .googleId(googleUserId)
-                .email(email)
-                .name(displayName)
-                .role(Role.CUSTOMER)
-                .lastLoginAt(Instant.now())
-                .build();
-    }
 }
