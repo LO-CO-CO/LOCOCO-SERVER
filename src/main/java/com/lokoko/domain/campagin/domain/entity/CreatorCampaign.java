f1c81531b96eacda29cbf6f2f6d1345fcc8d4760
unrookie21 <piresj0318@gmail.com>
2025-08-30 18:44:25 +0900

    FEAT #203 : CreatorCampaign 엔티티 생성
    해당 엔티티는, 어떤 "크리에이터" 가 어떤 "캠페인" 에 참여하였는지 정보를 포함하고 있는 엔티티입니다.
    
    크리에이터가 참여한 캠페인의 각종 상태 정보(APPROVED, REJECTED, PENDING) 도 함께 관리해야 하기 때문에 별도 테이블을 두게 되었습니다.

---
+++ src/main/java/com/lokoko/domain/campagin/domain/entity/CreatorCampaign.java
@@ -0,0 +1,84 @@
+package com.lokoko.domain.campagin.domain.entity;
+
+import com.lokoko.domain.campagin.domain.entity.enums.ParticipationStatus;
+import com.lokoko.domain.creator.domain.entity.Creator;
+import com.lokoko.global.common.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import java.time.Instant;
+
+import static jakarta.persistence.FetchType.*;
+
+@Getter
+@Entity
+@Table(name = "creator_campaign")
+@SuperBuilder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+
+public class CreatorCampaign extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "creator_id")
+    private Creator creator;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "campaign_id")
+    private Campaign campaign;
+
+    /**
+     * 크리에이터가 캠페인을 지원했을 때의 세부 상태 현황
+     * APPROVED(승인됨) , REJECTED(거절됨) , PENDING(대기중) 등....
+     */
+    @Enumerated(value = EnumType.STRING)
+    private ParticipationStatus status;
+
+    // 캠페인에 지원한 날짜
+    private Instant appliedAt;
+
+    // 배송지 입력 여부 및 시간
+    private boolean addressConfirmed;
+    private Instant addressConfirmedAt;
+
+    // 1차 리뷰 업로드 여부 및 시간
+    private boolean firstReviewSubmitted;
+    private Instant firstReviewSubmittedAt;
+
+    // 수정 요청 여부 및 시간
+    private boolean revisionRequested;
+    private Instant revisionRequestedAt;
+
+    // 2차 리뷰 업로드 여부 및 시간
+    private boolean secondReviewSubmitted;
+    private Instant secondReviewSubmittedAt;
+
+    public void changeAddressConfirmed(boolean addressConfirmed) {
+        this.addressConfirmed = addressConfirmed;
+    }
+
+    public void changeFirstReviewSubmitted(boolean firstReviewSubmitted) {
+        this.firstReviewSubmitted = firstReviewSubmitted;
+    }
+
+    public void changeRevisionRequested(boolean revisionRequested) {
+        this.revisionRequested = revisionRequested;
+    }
+
+    public void changeSecondReviewSubmitted(boolean secondReviewSubmitted) {
+        this.secondReviewSubmitted = secondReviewSubmitted;
+    }
+
+    public void changeStatus(ParticipationStatus newStatus) {
+        this.status = newStatus;
+    }
+
+
+
+}
