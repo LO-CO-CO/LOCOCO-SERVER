066bb26b56e48698a3c24cbe0593103e832c61a1
huncozyboy <leejh5838@naver.com>
2025-09-01 23:27:31 +0900

    feat : 소셜클립 엔티티 생성

---
+++ src/main/java/com/lokoko/domain/socialclip/domain/SocialClip.java
@@ -0,0 +1,23 @@
+package com.lokoko.domain.socialclip.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Entity
+@SuperBuilder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class SocialClip {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "socialclip_id")
+    private Long id;
+}
