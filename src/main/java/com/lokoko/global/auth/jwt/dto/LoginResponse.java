af75f0a9d79767a401d6da0196b38dcbd0f9c4e3
Hyoeun <joohyoeun@sookmyung.ac.kr>
2025-09-02 20:13:04 +0900

    MOD #200 : role을 포함한 정적팩토리 메서드 추가 구현

--- src/main/java/com/lokoko/global/auth/jwt/dto/LoginResponse.java
+++ src/main/java/com/lokoko/global/auth/jwt/dto/LoginResponse.java
@@ -1,5 +1,6 @@
 package com.lokoko.global.auth.jwt.dto;
 
+import com.lokoko.domain.user.domain.entity.enums.Role;
 import com.lokoko.global.auth.entity.enums.OauthLoginStatus;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
@@ -17,7 +18,9 @@ public record LoginResponse(
         @Schema(requiredMode = REQUIRED)
         Long userId,
         @Schema(requiredMode = REQUIRED)
-        String tokenId
+        String tokenId,
+        @Schema(requiredMode = REQUIRED)
+        Role role
 ) {
     public static LoginResponse of(
             String accessToken,
@@ -26,6 +29,13 @@ public record LoginResponse(
             Long userId,
             String tokenId
     ) {
-        return new LoginResponse(accessToken, refreshToken, loginStatus, userId, tokenId);
+        return new LoginResponse(accessToken, refreshToken, loginStatus, userId, tokenId, null);
+    }
+
+    // role 포함
+    public static LoginResponse withRole(String accessToken, String refreshToken,
+                                   OauthLoginStatus loginStatus, Long userId,
+                                   String tokenId, Role role) {
+        return new LoginResponse(accessToken, refreshToken, loginStatus, userId, tokenId, role);
     }
 }
