c173a243d7209a004996b6eb90626cacaeb8eb98
Hyoeun <joohyoeun@sookmyung.ac.kr>
2025-09-02 20:13:25 +0900

    MOD #200 : INFO_REQUIRED 상태 추가

--- src/main/java/com/lokoko/global/auth/entity/enums/OauthLoginStatus.java
+++ src/main/java/com/lokoko/global/auth/entity/enums/OauthLoginStatus.java
@@ -6,7 +6,8 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum OauthLoginStatus {
-    LOGIN("소셜 로그인"),
+    LOGIN("소셜 로그인 최종 완료"),
+    INFO_REQUIRED("추가 정보 입력 필요"),
     REGISTER("최초 소셜 회원가입");
 
     private final String displayName;
