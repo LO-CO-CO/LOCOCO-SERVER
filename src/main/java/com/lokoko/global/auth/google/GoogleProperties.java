3b2de81a79fe6c1f18c8ab38e5be90013fbf0fe1
Hyoeun <joohyoeun@sookmyung.ac.kr>
2025-09-03 19:57:34 +0900

    mod #200 : GoogleProperties class를 record로 변경

--- src/main/java/com/lokoko/global/auth/google/GoogleProperties.java
+++ src/main/java/com/lokoko/global/auth/google/GoogleProperties.java
@@ -1,18 +1,13 @@
 package com.lokoko.global.auth.google;
 
-import lombok.Getter;
-import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
 
-@Component
 @ConfigurationProperties(prefix = "google")
-@Getter
-@Setter
-public class GoogleProperties {
-    private String clientId;
-    private String clientSecret;
-    private String redirectUri;
-    private String baseUrl;
-    private String scope;
+public record GoogleProperties(
+        String clientId,
+        String clientSecret,
+        String redirectUri,
+        String baseUrl,
+        String scope
+) {
 }
