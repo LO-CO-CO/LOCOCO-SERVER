3d3d8ecf28399243a7da36a7f470c5ff8c4701f7
huncozyboy <leejh5838@naver.com>
2025-09-03 14:41:34 +0900

    refactor : 소셜클립 엔티티 추가

--- src/main/java/com/lokoko/domain/socialclip/domain/SocialClip.java
+++ src/main/java/com/lokoko/domain/socialclip/domain/SocialClip.java
@@ -1,7 +1,10 @@
 package com.lokoko.domain.socialclip.domain;
 
+import com.lokoko.domain.socialclip.domain.entity.enums.SocialClipContent;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -20,4 +23,32 @@ public class SocialClip {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "social_clip_id")
     private Long id;
+
+    @Column
+    Long instaPlays;
+
+    @Column
+    Long instaLikes;
+
+    @Column
+    Long instaComments;
+
+    @Column
+    Long instaShares;
+
+    @Column
+    Long youtubePlays;
+
+    @Column
+    Long youtubeLikes;
+
+    @Column
+    Long youtubeComments;
+
+    @Column
+    Long youtubeShares;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private SocialClipContent socialClipContent;
 }
