292f9ba9a1098f8d659d72e2f013b9a376f34025
Hyoeun <joohyoeun@sookmyung.ac.kr>
2025-09-06 01:23:23 +0900

    mod #209 : SkinTone, SkinType enum creator로 위치 이동

---
+++ src/main/java/com/lokoko/domain/creator/domain/entity/enums/SkinTone.java
@@ -0,0 +1,30 @@
+package com.lokoko.domain.creator.domain.entity.enums;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum SkinTone {
+    SHADE_1("shade 1"),
+    SHADE_2("shade 2"),
+    SHADE_3("shade 3"),
+    SHADE_4("shade 4"),
+    SHADE_5("shade 5"),
+    SHADE_6("shade 6"),
+    SHADE_7("shade 7"),
+    SHADE_8("shade 8"),
+    SHADE_9("shade 9"),
+    SHADE_10("shade 10"),
+    SHADE_11("shade 11"),
+    SHADE_12("shade 12"),
+    SHADE_13("shade 13"),
+    SHADE_14("shade 14"),
+    SHADE_15("shade 15"),
+    SHADE_16("shade 16"),
+    SHADE_17("shade 17"),
+    SHADE_18("shade 18"),
+    SHADE_19("shade 19"),
+    SHADE_20("shade 20");
+    private final String displayName;
+}
