45b90fea90bc7988fb9b895a5fac27c8318ec1f9
unrookie21 <piresj0318@gmail.com>
2025-09-07 11:43:40 +0900

    FEAT #211 : 캠페인 상세조회를 위한 컨트롤러 구현

--- src/main/java/com/lokoko/domain/campaign/api/CampaignController.java
+++ src/main/java/com/lokoko/domain/campaign/api/CampaignController.java
@@ -1,9 +1,18 @@
 package com.lokoko.domain.campaign.api;
 
+import com.lokoko.domain.campaign.api.dto.response.CampaignDetailResponse;
+import com.lokoko.domain.campaign.api.message.ResponseMessage;
 import com.lokoko.domain.campaign.application.service.CampaignReadService;
 import com.lokoko.domain.campaign.application.service.CampaignService;
+import com.lokoko.global.auth.annotation.CurrentUser;
+import com.lokoko.global.common.response.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,5 +24,17 @@ public class CampaignController {
 
     private final CampaignService campaignService;
     private final CampaignReadService campaignReadService;
+
+    @Operation(summary = "캠페인 상세 조회")
+    @GetMapping("/{campaignId}")
+    public ApiResponse<CampaignDetailResponse> getCampaignDetail(
+            @Parameter(hidden = true) @CurrentUser Long creatorId,
+            @PathVariable Long campaignId) {
+
+        CampaignDetailResponse response = campaignReadService.getCampaignDetail(creatorId, campaignId);
+        return ApiResponse.success(HttpStatus.OK, ResponseMessage.CAMPAIGN_DETAIL_GET_SUCCESS.getMessage(), response);
+    }
+
+
 
 }
