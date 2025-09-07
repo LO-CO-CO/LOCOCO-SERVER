2cc0fa42b65eb83a6e03868f442e31dc8d378f08
unrookie21 <piresj0318@gmail.com>
2025-09-07 11:42:03 +0900

    FEAT #211 : 캠페인 상세조회를 위한 repository 계층 구현
    1. CampaignRepository 에서는, 캠페인과 브랜드를 fetch join 하여 조회합니다.
    
    2. CampaignImageRepository 에서는, 캠페인과 관련된 TOP, BOTTOM 이미지를 조회합니다.
    
    3. CreatorCampaignRepository 에서는, (크리에이터-캠페인) 참여정보를 조회합니다.

---
+++ src/main/java/com/lokoko/domain/image/domain/repository/CampaignImageRepository.java
@@ -0,0 +1,26 @@
+package com.lokoko.domain.image.domain.repository;
+
+import com.lokoko.domain.campaign.api.dto.response.CampaignImageResponse;
+import com.lokoko.domain.image.domain.entity.CampaignImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface CampaignImageRepository extends JpaRepository<CampaignImage, Long> {
+
+    @Query("SELECT new com.lokoko.domain.campaign.api.dto.response.CampaignImageResponse(ci.id, ci.url, ci.displayOrder) " +
+            "FROM CampaignImage ci " +
+            "WHERE ci.campaign.id = :campaignId " +
+            "AND ci.imageType = com.lokoko.domain.image.domain.entity.enums.ImageType.TOP " +
+            "ORDER BY ci.displayOrder")
+    List<CampaignImageResponse> findTopImagesByCampaignId(@Param("campaignId") Long campaignId);
+
+    @Query("SELECT new com.lokoko.domain.campaign.api.dto.response.CampaignImageResponse(ci.id, ci.url, ci.displayOrder) " +
+            "FROM CampaignImage ci " +
+            "WHERE ci.campaign.id = :campaignId " +
+            "AND ci.imageType = com.lokoko.domain.image.domain.entity.enums.ImageType.BOTTOM " +
+            "ORDER BY ci.displayOrder")
+    List<CampaignImageResponse> findBottomImagesByCampaignId(@Param("campaignId") Long campaignId);
+}
