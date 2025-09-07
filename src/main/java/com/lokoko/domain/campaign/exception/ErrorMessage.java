9591716af02fdafa01b0a1268ae4a39a4719b77b
unrookie21 <piresj0318@gmail.com>
2025-09-07 11:43:31 +0900

    FEAT #211 : 캠페인 상세조회를 위한 예외처리 추가

--- src/main/java/com/lokoko/domain/campaign/exception/ErrorMessage.java
+++ src/main/java/com/lokoko/domain/campaign/exception/ErrorMessage.java
@@ -7,7 +7,8 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ErrorMessage {
 
-    CAMPAIGN_NOT_FOUND("존재하지 않는 캠페인입니다.");
+    CAMPAIGN_NOT_FOUND("존재하지 않는 캠페인입니다."),
+    INVALID_CAMPAIGN_STATUS("해당 캠페인은 조회 가능한 상태가 아닙니다. (미승인 / 작성 상태)");
 
     private final String message;
 }
