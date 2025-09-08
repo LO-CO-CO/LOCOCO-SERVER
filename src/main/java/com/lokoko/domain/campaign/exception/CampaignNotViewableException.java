0f2d503d0cb7c561825dd8db9ced32336443b0bf
unrookie21 <piresj0318@gmail.com>
2025-09-08 19:06:12 +0900

    REFACTOR #211 : 캠페인 상세조회 관련 예외의 이름 변경
    캠페인 상세 조회에서, DRAFT, WAITING_APPROVAL 상태인 캠페인은 조회가 되면 안 됩니다.
    위와 같은 상황을 처리하는 예외의 이름을 CampaignNotViewableException 으로 수정했습니다.
    
    이전 예외는 권한 관련 예외의 이름을 암시하고 있어 , 예외명이 적합하지 않았습니다.

--- src/main/java/com/lokoko/domain/campaign/exception/CampaignNotViewableException.java
+++ src/main/java/com/lokoko/domain/campaign/exception/CampaignNotViewableException.java
@@ -3,8 +3,8 @@ package com.lokoko.domain.campaign.exception;
 import com.lokoko.global.common.exception.BaseException;
 import org.springframework.http.HttpStatus;
 
-public class CampaignNotAccessibleException extends BaseException {
-    public CampaignNotAccessibleException() {
+public class CampaignNotViewableException extends BaseException {
+    public CampaignNotViewableException() {
         super(HttpStatus.BAD_REQUEST, ErrorMessage.INVALID_CAMPAIGN_STATUS.getMessage());
     }
 }
