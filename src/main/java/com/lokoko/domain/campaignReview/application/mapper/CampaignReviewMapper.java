47eaf0837fc064dddfc65540da5fc779638b94c9
huncozyboy <leejh5838@naver.com>
2025-09-11 05:15:56 +0900

    feat #208 : 크리에이터 리뷰 컨트롤러, 매퍼 구현

---
+++ src/main/java/com/lokoko/domain/campaignReview/application/mapper/CampaignReviewMapper.java
@@ -0,0 +1,41 @@
+package com.lokoko.domain.campaignReview.application.mapper;
+
+import com.lokoko.domain.campaign.domain.entity.CreatorCampaign;
+import com.lokoko.domain.campaignReview.api.dto.request.FirstReviewUploadRequest;
+import com.lokoko.domain.campaignReview.api.dto.request.SecondReviewUploadRequest;
+import com.lokoko.domain.campaignReview.api.dto.response.ReviewUploadResponse;
+import com.lokoko.domain.campaignReview.domain.entity.CampaignReview;
+import com.lokoko.domain.campaignReview.domain.entity.enums.ReviewRound;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CampaignReviewMapper {
+
+    public CampaignReview toFirstReview(CreatorCampaign creatorCampaign,
+                                        FirstReviewUploadRequest request) {
+        CampaignReview campaignReview = new CampaignReview();
+        campaignReview.bindToCreatorCampaign(creatorCampaign);
+        campaignReview.designateRound(ReviewRound.FIRST);
+        campaignReview.chooseContentType(request.content());
+        campaignReview.requestFirstReview(request.captionWithHashtags());
+        return campaignReview;
+    }
+
+    public CampaignReview toSecondReview(CreatorCampaign creatorCampaign,
+                                         SecondReviewUploadRequest secondReviewUploadRequest) {
+        CampaignReview campaignReview = new CampaignReview();
+        campaignReview.bindToCreatorCampaign(creatorCampaign);
+        campaignReview.designateRound(ReviewRound.SECOND);
+        campaignReview.chooseContentType(secondReviewUploadRequest.content());
+        campaignReview.requestModification(secondReviewUploadRequest.brandNote());
+        campaignReview.requestSecondReview(secondReviewUploadRequest.captionWithHashtags(),
+                secondReviewUploadRequest.postUrl());
+        return campaignReview;
+    }
+
+    public ReviewUploadResponse toUploadResponse(CampaignReview saved) {
+        return ReviewUploadResponse.builder()
+                .reviewId(saved.getId())
+                .build();
+    }
+}
