e2715e24cdbc8ba8abf352e54800fab11c94f946
huncozyboy <leejh5838@naver.com>
2025-09-11 05:13:19 +0900

    feat #208 : 크리에이터 관련 예외처리 추가

---
+++ src/main/java/com/lokoko/domain/creator/exception/CreatorNotFoundException.java
@@ -0,0 +1,12 @@
+package com.lokoko.domain.creator.exception;
+
+import static com.lokoko.domain.creator.exception.ErrorMessage.CREATOR_NOT_FOUND;
+
+import com.lokoko.global.common.exception.BaseException;
+import org.springframework.http.HttpStatus;
+
+public class CreatorNotFoundException extends BaseException {
+    public CreatorNotFoundException() {
+        super(HttpStatus.NOT_FOUND, CREATOR_NOT_FOUND.getMessage());
+    }
+}
