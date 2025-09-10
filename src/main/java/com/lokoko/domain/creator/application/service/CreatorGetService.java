17612fb482b8bb221dab331de14d21354687716a
huncozyboy <leejh5838@naver.com>
2025-09-11 06:32:52 +0900

    FEAT #208 : 마이페이지 조회, 수정 비즈니스 로직 구현

--- src/main/java/com/lokoko/domain/creator/application/service/CreatorGetService.java
+++ src/main/java/com/lokoko/domain/creator/application/service/CreatorGetService.java
@@ -2,8 +2,6 @@ package com.lokoko.domain.creator.application.service;
 
 import com.lokoko.domain.campaign.domain.entity.CreatorCampaign;
 import com.lokoko.domain.campaign.domain.repository.CreatorCampaignRepository;
-import com.lokoko.domain.creator.api.dto.response.CreatorMyPageResponse;
-import com.lokoko.domain.creator.application.mapper.CreatorMapper;
 import com.lokoko.domain.creator.domain.entity.Creator;
 import com.lokoko.domain.creator.domain.repository.CreatorRepository;
 import com.lokoko.domain.creator.exception.CreatorCampaignNotFoundException;
@@ -19,20 +17,16 @@ public class CreatorGetService {
 
     private final CreatorRepository creatorRepository;
     private final CreatorCampaignRepository creatorCampaignRepository;
-    private final CreatorMapper creatorMapper;
 
     public Creator findByUserId(Long userId) {
         return creatorRepository.findByUserId(userId)
                 .orElseThrow(CreatorNotFoundException::new);
     }
 
-    public CreatorCampaign findParticipation(Long campaignId, Long creatorId) {
-        return creatorCampaignRepository.findByCreatorIdAndCampaignId(campaignId, creatorId)
-                .orElseThrow(CreatorCampaignNotFoundException::new);
-    }
+    public CreatorCampaign findParticipation(Long creatorId, Long campaignId) {
 
-    public CreatorMyPageResponse getMyProfile(Long userId) {
-        Creator creator = findByUserId(userId);
-        return creatorMapper.toMyPageResponse(creator);
+        return creatorCampaignRepository
+                .findByCampaign_IdAndCreator_Id(campaignId, creatorId)
+                .orElseThrow(CreatorCampaignNotFoundException::new);
     }
 }
