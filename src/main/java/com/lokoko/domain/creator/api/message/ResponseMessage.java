7cf5dd49ddf4be2e51c583d3956c24a115fd15d6
Hyoeun <joohyoeun@sookmyung.ac.kr>
2025-09-12 05:30:14 +0900

    mod #207 : Controller, ResponseMessage 수정

--- src/main/java/com/lokoko/domain/creator/api/message/ResponseMessage.java
+++ src/main/java/com/lokoko/domain/creator/api/message/ResponseMessage.java
@@ -8,7 +8,7 @@ import lombok.Getter;
 public enum ResponseMessage {
     CREATOR_ID_CHECK_SUCCESS("사용 가능한 크리에이터 ID입니다."),
     CREATOR_INFO_UPDATE_SUCCESS("크리에이터 추가 정보 입력을 성공했습니다."),
-    CREATOR_SNS_STATE_SUCCESS("크리에이터 SNS 연결 상태를 성공적으로 불러왔습니다"),
+    CREATOR_GET_SNS_STATUS_SUCCESS("크리에이터 SNS 연결 상태를 성공적으로 불러왔습니다"),
     CREATOR_GET_INFO_REGISTER_SUCCESS("회원가입시에 입력한 필드를 성공적으로 불러왔습니다."),
     CREATOR_LOGIN_SUCCESS("크리에이터 최종 회원가입이 성공했습니다"),
 
