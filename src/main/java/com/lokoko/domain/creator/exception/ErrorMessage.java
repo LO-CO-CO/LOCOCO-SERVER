330d325af05e6495ccc9e18e7493e6b9d5bd1bb8
Hyoeun <joohyoeun@sookmyung.ac.kr>
2025-09-12 05:30:45 +0900

    feat #207 : exception 추가

--- src/main/java/com/lokoko/domain/creator/exception/ErrorMessage.java
+++ src/main/java/com/lokoko/domain/creator/exception/ErrorMessage.java
@@ -9,6 +9,7 @@ public enum ErrorMessage {
     CREATOR_ID_ALREADY_EXIST("이미 존재하는 크리에이터 ID입니다."),
     SNS_NOT_CONNECTED("SNS는 필수적으로 1개 이상 연결되어야 합니다."),
     CREATOR_BASIC_INFO_NOT_COMPLETED("크리에이터 필수 정보 입력이 아직 완료되지 않은 상태입니다."),
+    ONLY_CREATOR_ROLE_SIGN_UP("오직 크리에이터만 가입할 수 있습니다"),
 
     // CampaignReview 관련
     CREATOR_NOT_FOUND("크리에이터가 존재하지 않습니다."),
