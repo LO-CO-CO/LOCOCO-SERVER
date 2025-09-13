a0d9a555101fd9dbb40412495e20c5fc6c8aec89
unrookie21 <piresj0318@gmail.com>
2025-09-13 23:43:05 +0900

    REFACTOR #213 : 크리에이터 조회 및 예외 호출 메소드를 레포지토리의 default 메소드로 추가
    크리에이터를 조회하고, 예외를 던지는 로직이 반복적으로 나타나기 때문에 해당 로직을 레포지토리 단에 구성해주었습니다.

--- src/main/java/com/lokoko/domain/creator/domain/repository/CreatorRepository.java
+++ src/main/java/com/lokoko/domain/creator/domain/repository/CreatorRepository.java
@@ -2,6 +2,8 @@ package com.lokoko.domain.creator.domain.repository;
 
 import com.lokoko.domain.creator.domain.entity.Creator;
 import java.util.Optional;
+
+import com.lokoko.domain.creator.exception.CreatorNotFoundException;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +11,9 @@ import org.springframework.stereotype.Repository;
 public interface CreatorRepository extends JpaRepository<Creator, Long> {
 
     Optional<Creator> findByUserId(Long userId);
+
+    default Creator findByIdOrThrow(Long creatorId) {
+        return findById(creatorId)
+                .orElseThrow(CreatorNotFoundException::new);
+    }
 }
