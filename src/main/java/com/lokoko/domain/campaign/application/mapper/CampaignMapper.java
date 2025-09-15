d0f692676a22d8e1b9cbe9bff5365bc67d2cdd2f
huncozyboy <leejh5838@naver.com>
2025-09-16 04:30:43 +0900

    MOD #215 : 리뷰 작성 뷰에서 반환해줄 참여한 캠페인 응답 DTO 패키지 경로 변경

--- src/main/java/com/lokoko/domain/campaign/application/mapper/CampaignMapper.java
+++ src/main/java/com/lokoko/domain/campaign/application/mapper/CampaignMapper.java
@@ -1,8 +1,8 @@
 package com.lokoko.domain.campaign.application.mapper;
 
+import com.lokoko.domain.campaign.api.dto.response.ParticipatingCampaignResponse;
 import com.lokoko.domain.campaign.domain.entity.Campaign;
 import com.lokoko.domain.campaign.domain.entity.CreatorCampaign;
-import com.lokoko.domain.campaignReview.api.dto.response.ParticipatingCampaignResponse;
 import org.springframework.stereotype.Component;
 
 @Component
