bd702581e6cdf141a83e5471fe24c8d01509171f
unrookie21 <piresj0318@gmail.com>
2025-09-16 23:23:59 +0900

    REFACTOR #223 : CampaignProductType enum 구성요소 변경
    GUI 화면에서, FACE_MAKEUP 과 LIP_MAKEUP 이 MAKEUP 으로 통일됨에 따라 enum 또한 변경하였습니다.

--- src/main/java/com/lokoko/domain/campaign/domain/entity/enums/CampaignProductType.java
+++ src/main/java/com/lokoko/domain/campaign/domain/entity/enums/CampaignProductType.java
@@ -1,8 +1,15 @@
 package com.lokoko.domain.campaign.domain.entity.enums;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum CampaignProductType {
-    SKINCARE,
-    SUNCARE,
-    FACE_MAKEUP,
-    LIP_MAKEUP
+
+    SKINCARE("Skincare"),
+    SUNCARE("Suncare"),
+    MAKEUP("Makeup");
+
+    private final String displayName;
 }
