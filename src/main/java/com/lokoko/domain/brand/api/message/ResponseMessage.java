e88c6cb5f2adfaaa0436de5dd7643f8838e33729
unrookie21 <piresj0318@gmail.com>
2025-09-17 02:36:16 +0900

    FEAT #224 : 에러 메시지 및 응답 메시지 구현

--- src/main/java/com/lokoko/domain/brand/api/message/ResponseMessage.java
+++ src/main/java/com/lokoko/domain/brand/api/message/ResponseMessage.java
@@ -11,7 +11,9 @@ public enum ResponseMessage {
 
     CAMPAIGN_PUBLISH_SUCCESS("캠페인이 발행되었습니다."),
     CAMPAIGN_DRAFT_SUCCESS("캠페인 임시저장이 완료되었습니다."),
-    CAMPAIGN_UPDATE_SUCCESS("캠페인 수정이 완료되었습니다.");
+    CAMPAIGN_UPDATE_SUCCESS("캠페인 수정이 완료되었습니다."),
+    REVISION_REQUEST_SUCCESS("브랜드 수정사항 전달이 완료되었습니다"),
+    REVISION_SAVE_SUCCESS("브랜드 수정 사항 임시 저장에 성공했습니다");
 
     private final String message;
 }
