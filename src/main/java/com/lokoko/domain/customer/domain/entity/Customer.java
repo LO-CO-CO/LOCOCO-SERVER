6219448c9b83a668045ed605343517a157538030
Hyoeun <joohyoeun@sookmyung.ac.kr>
2025-09-16 12:11:19 +0900

    mod #218 : Customer 엔티티에 필드 추가

--- src/main/java/com/lokoko/domain/customer/domain/entity/Customer.java
+++ src/main/java/com/lokoko/domain/customer/domain/entity/Customer.java
@@ -1,7 +1,14 @@
 package com.lokoko.domain.customer.domain.entity;
 
+import com.lokoko.domain.creator.domain.entity.enums.ContentLanguage;
+import com.lokoko.domain.creator.domain.entity.enums.Gender;
+import com.lokoko.domain.creator.domain.entity.enums.SkinTone;
+import com.lokoko.domain.creator.domain.entity.enums.SkinType;
 import com.lokoko.domain.user.domain.entity.User;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.MapsId;
@@ -12,6 +19,8 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
 
 @Getter
 @Entity
@@ -29,4 +38,114 @@ public class Customer {
     @JoinColumn(name = "user_id")
     private User user;
 
+    @Column
+    private String customerName;
+
+    @Column
+    private LocalDate birthDate;
+
+    @Enumerated(EnumType.STRING)
+    @Column
+    private Gender gender;
+
+    @Column
+    private String countryCode;
+
+    @Column
+    private String phoneNumber;
+
+    @Column
+    private String country;
+
+    // 주/도/광역시 등
+    @Column
+    private String stateOrProvince;
+
+    @Column
+    private String cityOrTown;
+
+    @Column
+    private String addressLine1;
+
+    @Column
+    private String addressLine2;
+
+    @Column
+    private String postalCode;
+
+    @Enumerated(EnumType.STRING)
+    private SkinType skinType;
+
+    @Enumerated(EnumType.STRING)
+    private SkinTone skinTone;
+
+    @Enumerated(EnumType.STRING)
+    @Column
+    private ContentLanguage contentLanguage = ContentLanguage.ENGLISH;
+
+    @Column
+    private String instaUserId;
+
+    @Column
+    private String tikTokUserId;
+
+    public String getCreatorPhoneNumber() {
+        return countryCode + phoneNumber;
+    }
+
+    public void assignCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public void assignBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public void assignGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    public void assignCountryCode(String countryCode) {
+        this.countryCode = countryCode;
+    }
+
+    public void assignPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void assignCountry(String country) {
+        this.country = country;
+    }
+
+    public void assignStateOrProvince(String stateOrProvince) {
+        this.stateOrProvince = stateOrProvince;
+    }
+
+    public void assignCityOrTown(String cityOrTown) {
+        this.cityOrTown = cityOrTown;
+    }
+
+    public void assignAddressLine1(String addressLine1) {
+        this.addressLine1 = addressLine1;
+    }
+
+    public void assignAddressLine2(String addressLine2) {
+        this.addressLine2 = addressLine2;
+    }
+
+    public void assignPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public void assignSkinType(SkinType skinType) {
+        this.skinType = skinType;
+    }
+
+    public void assignSkinTone(SkinTone skinTone) {
+        this.skinTone = skinTone;
+    }
+
+    public void assignContentLanguage(ContentLanguage contentLanguage) {
+        this.contentLanguage = contentLanguage;
+    }
 }
