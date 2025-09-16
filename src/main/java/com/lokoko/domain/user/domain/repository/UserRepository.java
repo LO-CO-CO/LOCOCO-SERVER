a92102f5a8c870505337f9c27375eef36c84b7df
unrookie21 <piresj0318@gmail.com>
2025-09-16 21:22:55 +0900

    DEL #213 : UserRepository 계층에서 예외를 던지지 않도록 메소드를 제거

--- src/main/java/com/lokoko/domain/user/domain/repository/UserRepository.java
+++ src/main/java/com/lokoko/domain/user/domain/repository/UserRepository.java
@@ -1,9 +1,6 @@
 package com.lokoko.domain.user.domain.repository;
 
-import com.lokoko.domain.creator.domain.entity.Creator;
-import com.lokoko.domain.creator.exception.CreatorNotFoundException;
 import com.lokoko.domain.user.domain.entity.User;
-import com.lokoko.domain.user.exception.UserNotFoundException;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -14,9 +11,5 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByGoogleId(String googleId);
     Optional<User> findByLineId(String lineId);
 
-    default User findByIdOrThrow(Long creatorId) {
-        return findById(creatorId)
-                .orElseThrow(UserNotFoundException::new);
-    }
 
 }
