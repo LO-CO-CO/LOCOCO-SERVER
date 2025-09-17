f21532c3040b11411f15413d0640861fd29b7b7f
unrookie21 <piresj0318@gmail.com>
2025-09-17 13:51:49 +0900

    REFACTOR #224 : 응답 DTO 에 @Shema 애노테이션 추가

--- src/main/java/com/lokoko/domain/campaign/api/dto/response/MainPageCampaignListResponse.java
+++ src/main/java/com/lokoko/domain/campaign/api/dto/response/MainPageCampaignListResponse.java
@@ -1,11 +1,16 @@
 package com.lokoko.domain.campaign.api.dto.response;
 
 import com.lokoko.global.common.response.PageableResponse;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 import java.util.List;
 
+import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.REQUIRED;
+
 public record MainPageCampaignListResponse(
+        @Schema(requiredMode = REQUIRED, description = "메인 페이지 캠페인 목록")
         List<MainPageCampaignResponse> campaigns,
+        @Schema(requiredMode = REQUIRED, description = "페이지 정보")
         PageableResponse pageInfo
 ) {
 }
--- src/main/java/com/lokoko/domain/campaign/api/dto/response/MainPageUpcomingCampaignListResponse.java
+++ src/main/java/com/lokoko/domain/campaign/api/dto/response/MainPageUpcomingCampaignListResponse.java
@@ -1,11 +1,16 @@
 package com.lokoko.domain.campaign.api.dto.response;
 
 import com.lokoko.global.common.response.PageableResponse;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 import java.util.List;
 
+import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.REQUIRED;
+
 public record MainPageUpcomingCampaignListResponse(
+        @Schema(requiredMode = REQUIRED, description = "메인 페이지 오픈 예정 캠페인 목록")
         List<MainPageUpcomingCampaignResponse> campaigns,
+        @Schema(requiredMode = REQUIRED, description = "페이지 정보")
         PageableResponse pageInfo
 ) {
 }
