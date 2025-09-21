895cef44557728a81c0e50cf70dfaf082412fd9a
unrookie21 <piresj0318@gmail.com>
2025-09-21 16:23:02 +0900

    CHORE #238 : 응답 DTO 에 @Schema 애노테이션 추가

--- src/main/java/com/lokoko/domain/campaign/api/dto/response/CampaignDetailResponse.java
+++ src/main/java/com/lokoko/domain/campaign/api/dto/response/CampaignDetailResponse.java
@@ -5,27 +5,44 @@ import com.lokoko.domain.campaign.domain.entity.Campaign;
 import com.lokoko.domain.campaign.domain.entity.enums.CampaignDetailPageStatus;
 import com.lokoko.domain.campaign.domain.entity.enums.CampaignLanguage;
 import com.lokoko.domain.campaign.domain.entity.enums.CampaignType;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 import java.time.Instant;
 import java.util.List;
 
 public record CampaignDetailResponse(
 
+        @Schema(requiredMode = Schema.RequiredMode.REQUIRED, description = "캠페인 id" , example = "1")
         Long campaignId,
+        @Schema(requiredMode = Schema.RequiredMode.REQUIRED, description = "캠페인 종류" , example = "GIVEAWAY")
         CampaignType campaignType,
+        @Schema(requiredMode = Schema.RequiredMode.REQUIRED, description = "캠페인 제목"  , example = "캠페인aa")
         String title,
+        @Schema(requiredMode = Schema.RequiredMode.REQUIRED, description = "브랜드 프로필 이미지 URL" , example = "https://lococo-bucket.s3.ap-northeast-2.amazonaws.com/image/image.png")
         String brandImageUrl,
+        @Schema(requiredMode = Schema.RequiredMode.REQUIRED, description = "브랜드 이름" , example = "브랜드A")
         String brandName,
+        @Schema(requiredMode = Schema.RequiredMode.REQUIRED, description = "캠페인 언어" , example = "ES, EN")
         CampaignLanguage language,
+        @Schema(requiredMode = Schema.RequiredMode.REQUIRED, description = "캠페인 지원 시작 날짜" , example = "2025-09-16T07:32:08.995Z")
         Instant applyStartDate,
+        @Schema(requiredMode = Schema.RequiredMode.REQUIRED, description = "캠페인 지원 마감 날짜", example = "2025-09-16T07:32:08.995Z" )
         Instant applyDeadline,
+        @Schema(requiredMode = Schema.RequiredMode.REQUIRED, description = "크리에이터 발표 날짜" , example = "2025-09-16T07:32:08.995Z")
         Instant creatorAnnouncementDate,
+        @Schema(requiredMode = Schema.RequiredMode.REQUIRED, description = "2차 리뷰 제출 마감일" , example = "2025-09-16T07:32:08.995Z")
         Instant reviewSubmissionDeadline,
+        @Schema(requiredMode = Schema.RequiredMode.REQUIRED, description = "크리에이터 컨텐츠 제출 요구사항 리스트")
         List<String> deliverableRequirements,
+        @Schema(requiredMode = Schema.RequiredMode.REQUIRED, description = "크리에이터 참요 조건 리스트")
         List<String> participationRewards,
+        @Schema(requiredMode = Schema.RequiredMode.REQUIRED, description = "크리에이터 자격 요건 리스트")
         List<String> eligibilityRequirements,
+        @Schema(requiredMode = Schema.RequiredMode.REQUIRED, description = "상단 이미지 목록 리스트")
         List<CampaignImageResponse> topImages,
+        @Schema(requiredMode = Schema.RequiredMode.REQUIRED, description = "하단 이미지 목록 리스트")
         List<CampaignImageResponse> bottomImages,
+        @Schema(requiredMode = Schema.RequiredMode.REQUIRED, description = "캠페인 상태" , example = "Coming Soon, Apply Now!, Completed ....")
         String campaignStatusCode
 ) {
 
