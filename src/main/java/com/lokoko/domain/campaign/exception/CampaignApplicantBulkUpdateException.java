cf333e33a08149fb782ab49b5fe0096f9d71b5c2
unrookie21 <piresj0318@gmail.com>
2025-09-21 11:49:48 +0900

    CHORE #233 : CampaignApplicantBulkUpdateException 의 Http 상태메시지를 500 -> 422(CONFLICT) 로 변경

--- src/main/java/com/lokoko/domain/campaign/exception/CampaignApplicantBulkUpdateException.java
+++ src/main/java/com/lokoko/domain/campaign/exception/CampaignApplicantBulkUpdateException.java
@@ -5,6 +5,6 @@ import org.springframework.http.HttpStatus;
 
 public class CampaignApplicantBulkUpdateException extends BaseException {
     public CampaignApplicantBulkUpdateException() {
-        super(HttpStatus.INTERNAL_SERVER_ERROR, ErrorMessage.CAMPAIGN_APPLICANT_BULK_UPDATE_FAILED.getMessage());
+        super(HttpStatus.CONFLICT, ErrorMessage.CAMPAIGN_APPLICANT_BULK_UPDATE_FAILED.getMessage());
     }
 }
