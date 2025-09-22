75292fcbc13c9f04b00576bd83a30c3749074796
huncozyboy <leejh5838@naver.com>
2025-09-22 18:00:07 +0900

    FEAT #246 : 리뷰 업로드 관련 도메인 추가

---
+++ src/main/java/com/lokoko/domain/campaignReview/domain/repository/CampaignReviewImageRepository.java
@@ -0,0 +1,14 @@
+package com.lokoko.domain.campaignReview.domain.repository;
+
+import com.lokoko.domain.campaignReview.domain.entity.CampaignReview;
+import com.lokoko.domain.campaignReview.domain.entity.CampaignReviewImage;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CampaignReviewImageRepository extends JpaRepository<CampaignReviewImage, Long> {
+    List<CampaignReviewImage> findAllByCampaignReview_IdOrderByIdAsc(Long campaignReviewId);
+
+    long countByCampaignReview_Id(Long campaignReviewId);
+
+    void deleteAllByCampaignReview(CampaignReview review);
+}
