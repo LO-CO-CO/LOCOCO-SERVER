7e6704d344f21cdc8ac01deaa4a1642b23e0f9f8
huncozyboy <leejh5838@naver.com>
2025-09-25 07:41:59 +0900

    FEAT #264 : 리뷰에 업로드된 미디어 URL 리스트를 displayOrder 오름차순으로 반환하는 메서드 구현

--- src/main/java/com/lokoko/domain/campaignReview/domain/repository/CampaignReviewVideoRepository.java
+++ src/main/java/com/lokoko/domain/campaignReview/domain/repository/CampaignReviewVideoRepository.java
@@ -4,6 +4,7 @@ import com.lokoko.domain.campaignReview.domain.entity.CampaignReview;
 import com.lokoko.domain.campaignReview.domain.entity.CampaignReviewVideo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CampaignReviewVideoRepository extends JpaRepository<CampaignReviewVideo, Long> {
+public interface CampaignReviewVideoRepository extends JpaRepository<CampaignReviewVideo, Long>,
+        CampaignReviewVideoRepositoryCustom {
     void deleteAllByCampaignReview(CampaignReview campaignReview);
 }
