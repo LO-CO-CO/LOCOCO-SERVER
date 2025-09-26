61acbd802804f1f1ada653a0c78d09d5f0eb13f2
huncozyboy <leejh5838@naver.com>
2025-09-27 03:48:36 +0900

    CHORE #222 : 환경변수 추가

--- src/main/java/com/lokoko/global/auth/provider/insta/config/InstaProperties.java
+++ src/main/java/com/lokoko/global/auth/provider/insta/config/InstaProperties.java
@@ -2,7 +2,7 @@ package com.lokoko.global.auth.provider.insta.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-@ConfigurationProperties(prefix = "insta")
+@ConfigurationProperties(prefix = "instagram")
 public record InstaProperties(
         String clientId,
         String clientSecret,
