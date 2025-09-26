3c5fade68bdc7204a7d18406cf6df77bb055419f
huncozyboy <leejh5838@naver.com>
2025-09-26 09:08:09 +0900

    MOD #264 : 리뷰 업로드 관련 예외처리 구현

--- src/main/java/com/lokoko/domain/campaignReview/exception/ErrorMessage.java
+++ src/main/java/com/lokoko/domain/campaignReview/exception/ErrorMessage.java
@@ -13,7 +13,14 @@ public enum ErrorMessage {
     MISMATCHED_CONTENT_TYPE("2차 리뷰 콘텐츠 타입은, 1차 리뷰 콘텐츠 타입과 동일해야 합니다."),
     FIRST_REVIEW_NOT_FOUND("1차 리뷰가 있어야 2차 리뷰를 작성할 수 있습니다."),
     CAMPAIGN_REVIEW_NOT_FOUND("캠페인에 대한 리뷰가 존재하지 않습니다"),
-    REVISION_REQUEST_NOT_ALLOWED("1차 리뷰가 아닌 리뷰에는 브랜드가 수정 사항을 남길 수 없습니다");
+    REVISION_REQUEST_NOT_ALLOWED("1차 리뷰가 아닌 리뷰에는 브랜드가 수정 사항을 남길 수 없습니다"),
+
+    MISSING_PLATFORM("컨텐츠 플랫폼 설정이 누락되었습니다."),
+    DUPLICATED_PLATFORM("컨텐츠 플랫폼 설정이 중복되었습니다."),
+    INVALID_TWO_SET_COMBINATION("두 세트 캠페인은 INSTA_REELS/TIKTOK_VIDEO 조합만 허용됩니다."),
+    FIRST_SET_REQUIRED("첫 번째 세트가 비어 있습니다."),
+    SECOND_SET_REQUIRED("두 번째 세트가 비어 있습니다."),
+    SECOND_SET_NOT_ALLOWED("두 번째 세트는 제공되면 안 됩니다.");
 
     private final String message;
 }
