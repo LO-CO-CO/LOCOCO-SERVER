f3ba68d11ed1d155ad5cc08a00149e882412386f
unrookie21 <piresj0318@gmail.com>
2026-01-03 16:54:23 +0900

    FEAT #427 : ADMIN RUD API - Usecase 구현

--- src/main/java/com/lokoko/domain/user/application/usecase/AdminUsecase.java
+++ src/main/java/com/lokoko/domain/user/application/usecase/AdminUsecase.java
@@ -1,14 +1,19 @@
 package com.lokoko.domain.user.application.usecase;
 
+import com.lokoko.domain.campaign.api.dto.response.CampaignBasicResponse;
 import com.lokoko.domain.campaign.application.service.CampaignGetService;
-import com.lokoko.domain.user.application.service.AdminCampaignUpdateService;
-import com.lokoko.domain.user.application.service.AdminCreatorUpdateService;
-import com.lokoko.domain.user.application.service.AdminReviewDeleteService;
-import com.lokoko.domain.user.application.service.UserGetService;
+import com.lokoko.domain.campaign.domain.repository.CampaignRepository;
+import com.lokoko.domain.user.api.dto.request.ApproveCampaignIdsRequest;
+import com.lokoko.domain.user.api.dto.request.ApprovedStatus;
+import com.lokoko.domain.user.api.dto.request.CampaignModifyRequest;
+import com.lokoko.domain.user.api.dto.request.DeleteCampaignIdsRequest;
+import com.lokoko.domain.user.api.dto.response.AdminCampaignListResponse;
+import com.lokoko.domain.user.application.service.*;
 import com.lokoko.domain.user.domain.entity.User;
 import com.lokoko.global.utils.AdminValidator;
 import java.time.Instant;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -22,28 +27,59 @@ public class AdminUsecase {
     private final AdminCampaignUpdateService adminCampaignUpdateService;
     private final AdminCreatorUpdateService adminCreatorUpdateService;
     private final AdminReviewDeleteService adminReviewDeleteService;
+    private final AdminCampaignDeleteService adminCampaignDeleteService;
+
+    private final CampaignRepository campaignRepository;
+
+    private void validateIsAdmin(Long userId) {
+        User user = userGetService.findUserById(userId);
+        AdminValidator.validateAdminRole(user);
+    }
 
     @Transactional
     public void approveCampaign(Long userId, Long campaignId) {
-        User user = userGetService.findUserById(userId);
-        AdminValidator.validateAdminRole(user);
-
+        validateIsAdmin(userId);
         campaignGetService.findByCampaignId(campaignId);
         adminCampaignUpdateService.approve(campaignId, Instant.now());
     }
 
     @Transactional
     public void approveCreator(Long adminUserId, Long userId) {
-        User user = userGetService.findUserById(adminUserId);
-        AdminValidator.validateAdminRole(user);
+        validateIsAdmin(adminUserId);
         adminCreatorUpdateService.approveById(userId, Instant.now());
     }
 
     @Transactional
-    public void deleteReviewByAdmin(Long UserId, Long campaignReviewId) {
-        User admin = userGetService.findUserById(UserId);
-        AdminValidator.validateAdminRole(admin);
+    public void deleteReviewByAdmin(Long userId, Long campaignReviewId) {
+        validateIsAdmin(userId);
+        adminReviewDeleteService.deleteReview(userId, campaignReviewId);
+    }
 
-        adminReviewDeleteService.deleteReview(UserId, campaignReviewId);
+    @Transactional
+    public void approveCampaigns(Long userId, ApproveCampaignIdsRequest request) {
+        validateIsAdmin(userId);
+        adminCampaignUpdateService.approveCampaigns(request.campaignIds());
+    }
+
+    @Transactional
+    public void deleteCampaigns(Long userId, DeleteCampaignIdsRequest request) {
+        validateIsAdmin(userId);
+        adminCampaignDeleteService.deleteCampaigns(request.campaignIds());
+    }
+
+    public AdminCampaignListResponse findAllCampaigns(Long userId, ApprovedStatus status, int page, int size) {
+        validateIsAdmin(userId);
+        return campaignRepository.findAllCampaignsByAdmin(status, PageRequest.of(page,size));
+    }
+
+    public CampaignBasicResponse findCampaignDetail(Long userId, Long campaignId) {
+        validateIsAdmin(userId);
+        return campaignGetService.getCampaignDetailForAdmin(campaignId);
+    }
+
+    @Transactional
+    public void modifyCampaign(Long userId, Long campaignId, CampaignModifyRequest request) {
+        validateIsAdmin(userId);
+        adminCampaignUpdateService.modifyCampaign(campaignId, request);
     }
 }
