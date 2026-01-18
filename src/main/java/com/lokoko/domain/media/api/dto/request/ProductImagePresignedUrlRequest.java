e96aa8ae36c9a017ef47201c6dc9986890453a00
Hyoeun <joohyoeun@sookmyung.ac.kr>
2026-01-18 21:15:25 +0900

    feat #439 : ProductImage 업로드 로직 구현

---
+++ src/main/java/com/lokoko/domain/media/api/dto/request/ProductImagePresignedUrlRequest.java
@@ -0,0 +1,16 @@
+package com.lokoko.domain.media.api.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
+import java.util.List;
+
+public record ProductImagePresignedUrlRequest(
+
+        @NotEmpty(message = "mediaType은 최소 1개 이상 필요합니다")
+        @Size(min = 1, max = 5, message = "mediaType은는 최대 5개까지 가능합니다")
+        List<@NotBlank(message = "mediaType은 비어있을 수 없습니다") String> mediaType
+
+) {
+}
---
+++ src/main/java/com/lokoko/domain/media/api/dto/response/ProductImagePresignedUrlResponse.java
@@ -0,0 +1,16 @@
+package com.lokoko.domain.media.api.dto.response;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
+
+import java.util.List;
+
+import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.REQUIRED;
+
+@Builder
+public record ProductImagePresignedUrlResponse(
+
+        @Schema(requiredMode = REQUIRED, description = "presignedUrl 리스트", example = "https://s3.ap-northeast-2.amazonaws.com/...")
+        List<String> mediaUrl
+) {
+}
