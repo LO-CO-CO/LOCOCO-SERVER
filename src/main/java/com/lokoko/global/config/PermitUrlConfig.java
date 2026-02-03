e12a74cd7ab212670ef54326476d04369ae6f1ba
unrookie21 <piresj0318@gmail.com>
2026-02-03 20:30:26 +0900

    FIX #460 : PermitUrlConfig 갱신
    /api/product-brand/products 엔드포인트를 publicUrl 에 추가하였습니다.

--- src/main/java/com/lokoko/global/config/PermitUrlConfig.java
+++ src/main/java/com/lokoko/global/config/PermitUrlConfig.java
@@ -25,7 +25,7 @@ public class PermitUrlConfig {
                 "/api/reviews/brands/videos",
                 "/api/reviews/brands/images",
                 "/api/product-brand",
-//                "/api/product-brand/products",
+                "/api/product-brand/products",
                 "/api/reviews/brands/summary"
         };
     }
