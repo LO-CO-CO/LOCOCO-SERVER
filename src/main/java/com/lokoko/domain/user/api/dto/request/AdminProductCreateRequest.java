2df70e0ae624d9cf95ecd164bb040a5ec73747c2
Hyoeun <joohyoeun@sookmyung.ac.kr>
2026-02-14 00:10:56 +0900

    unit nullable하게 변경

--- src/main/java/com/lokoko/domain/user/api/dto/request/AdminProductCreateRequest.java
+++ src/main/java/com/lokoko/domain/user/api/dto/request/AdminProductCreateRequest.java
@@ -31,7 +31,6 @@ public record AdminProductCreateRequest(
         Long normalPrice,
 
         @Schema(description = "용량 (최대 20자)", example = "30ml")
-        @NotBlank
         @Size(max = 20)
         String unit,
 
